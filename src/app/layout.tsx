aabd72578e34e2b92917598ebf8235b2027ff316
Rusyaidi <rsydmz@gmail.com>
2025-05-06 17:08:31 +0800

    fix: fixed issue on viewport in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export const metadata: Metadata = {
   description: "A portfolio showcasing my work as a Full Stack Developer, including B2B logistics platforms, web applications, and technical expertise in VueJS, NestJS, and more.",
   keywords: "Rusyaidi Mizi, Portfolio, Full Stack Developer, VueJS, NestJS, ReactJS, Web Development, B2B Logistics, Software Engineering, Web APIs, JavaScript, TypeScript, MongoDB, GraphQL",
   authors: [{ name: "Rusyaidi" }],
-  viewport: "width=device-width, initial-scale=1.0",
   robots: "index, follow",
 };
 
